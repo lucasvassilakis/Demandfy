63a8ff91bf265d9d90c6782dc089d17931550026
lucasvassilakis <lucasvassilakis@gmail.com>
2026-08-10 07:44:05 -0400

    Envio dos arquivos iniciais

---
+++ app/Views/sidebar.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <nav class="navbar sidebar">
+            <a class="navbar-brand" href="<?=URL?>"><img src="" alt=""></a>
+            <div class="" id="">
+                <!-- Menu alinhado à esquerda -->
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=URL?>" data-tooltip="tooltip" title="Página Inicial">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=URL?>/paginas/sobre" data-tooltip="tooltip" title="Sobre nós">Sobre nós</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?=URL?>/paginas/contato" data-tooltip="tooltip" title="contato">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
